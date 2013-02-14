ee90190622b49d79c71f2349586d574300672dea
andgalva <and.galva@gmail.com>
2013-02-14 22:51:39 +0100

    Chat dà errore su Message.persist

--- GAS_Roo/src/main/java/it/polito/ai/gas/Utils.java
+++ GAS_Roo/src/main/java/it/polito/ai/gas/Utils.java
@@ -1,6 +1,7 @@
 package it.polito.ai.gas;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Utils {
@@ -9,6 +10,19 @@ public class Utils {
 			return new HashSet<T>();
 		}else if( one ==null){
 			return two;
+		}else if(two==null){
+			return one;
+		}else{
+			HashSet<T> s=new HashSet<T>(one);
+			s.addAll(two);
+			return s;
+		}
+	}
+	public static <T> Set<T> merge(Set<T> one,List<T> two){
+		if(one==null&&two==null){
+			return new HashSet<T>();
+		}else if( one ==null){
+			return new HashSet<T>(two);
 		}else if(two==null){
 			return one;
 		}else{
@@ -26,4 +40,14 @@ public class Utils {
 		}
 	}
 
+	public static <T> Set<T> merge(Set<T> set, T obj) {
+		HashSet<T> s;
+		if (set == null)
+			s = new HashSet<T>();
+		else
+			s = new HashSet<T>(set);
+		if (obj != null)
+			s.add(obj);
+		return s;
+	}
 }
