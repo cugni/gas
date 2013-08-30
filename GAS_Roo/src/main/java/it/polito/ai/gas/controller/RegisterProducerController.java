6923ef2c3f2270fae152c9bf99ae84cf459451d4
andgalva <and.galva@gmail.com>
2013-08-30 19:02:02 +0200

    producer register (work in progress)
    ci siamo quasi dai dai dai dai

--- GAS_Roo/src/main/java/it/polito/ai/gas/controller/RegisterProducerController.java
+++ GAS_Roo/src/main/java/it/polito/ai/gas/controller/RegisterProducerController.java
@@ -3,11 +3,9 @@ package it.polito.ai.gas.controller;
 import it.polito.ai.gas.business.*;
 import org.joda.time.format.DateTimeFormat;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.UriUtils;
@@ -18,43 +16,46 @@ import javax.validation.Valid;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
-@RequestMapping("/register")
+@RequestMapping("/registerProducer")
 @Controller
-public class RegisterController {
+public class RegisterProducerController {
 
 
     @RequestMapping(params = "form",produces = "text/html")
     public String createRegisterForm(Model uiModel) {
-        populateEditForm(uiModel, new User());
-        return "/register/form";
+        populateEditForm(uiModel, new Producer());
+        return "/registerProducer/form";
     }
 
 
     @RequestMapping(method = RequestMethod.POST, produces = "text/html")
-    public String register(@Valid User user, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
-        user.setApproved(false);
-        user.setRole(UserType.ROLE_USER);    //TO-DO ...e il delegate?
+    public String register(@Valid Producer producer, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+
         if (bindingResult.hasErrors()) {
-            populateEditForm(uiModel, user);
-            return "/register/form";
+            populateEditForm(uiModel, producer);
+            return "/registerProducer/form";
         }
-        user.persist();
+        producer.persist();
+
+        User user = User.findUser(producer.getId());
+
 
         uiModel.asMap().clear();
+        uiModel.addAttribute("user", user);
         uiModel.addAttribute("itemId", user.getId());
         return "/success";
     }
 
-
-
-    void populateEditForm(Model uiModel, User user) {
-        uiModel.addAttribute("user", user);
+    void populateEditForm(Model uiModel, Producer producer) {
+        uiModel.addAttribute("producer", producer);
         addDateTimeFormatPatterns(uiModel);
         uiModel.addAttribute("deliverywithdrawals", DeliveryWithdrawal.findAllDeliveryWithdrawals());
         uiModel.addAttribute("events", Event.findAllEvents());
         uiModel.addAttribute("messages", Message.findAllMessages());
         uiModel.addAttribute("producers", Producer.findAllProducers());
+        uiModel.addAttribute("products", Product.findAllProducts());
         uiModel.addAttribute("purchaserequests", PurchaseRequest.findAllPurchaseRequests());
+        uiModel.addAttribute("users", User.findAllUsers());
         uiModel.addAttribute("usertypes", Arrays.asList(UserType.values()));
     }
 
