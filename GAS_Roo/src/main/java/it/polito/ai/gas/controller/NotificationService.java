8079ca53046b4a3545032ed3b5d1f05692f6e694
ccugnasc <ccugnasc@localhost.localdomain>
2013-09-09 02:16:56 +0200

    molte cose! Please enter the commit message for your changes. Lines starting

---
+++ GAS_Roo/src/main/java/it/polito/ai/gas/controller/NotificationService.java
@@ -0,0 +1,15 @@
+package it.polito.ai.gas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@RequestMapping("/notification.websocket")
+@Service
+
+
+public class NotificationService {
+}
