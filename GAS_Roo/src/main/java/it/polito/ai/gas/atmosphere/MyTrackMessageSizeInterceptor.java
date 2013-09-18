96d126cebbb590614864f415d502fa2df120edec
Cesare Cugnasco <cesare.cugnasco@gmail.com>
2013-09-18 02:22:03 +0200

    maremma atmosfera

---
+++ GAS_Roo/src/main/java/it/polito/ai/gas/atmosphere/MyTrackMessageSizeInterceptor.java
@@ -0,0 +1,21 @@
+package it.polito.ai.gas.atmosphere;
+
+import org.atmosphere.cpr.Action;
+import org.atmosphere.cpr.AtmosphereResource;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cesare
+ * Date: 18/09/13
+ * Time: 01:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyTrackMessageSizeInterceptor extends org.atmosphere.client.TrackMessageSizeInterceptor{
+    @Override
+    public Action inspect(AtmosphereResource r) {
+        if (! r.getRequest().getRequestURI().matches("/not/[0-9][1-9]*")){
+            return    Action.CONTINUE;
+        }
+        return super.inspect(r);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
